dd3f589ff0f6c7fdd4f208158d14239a53615271
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2018-03-26 11:32:23 -0500

    adding excessive methods because already handled by StringUtils class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,21 +19,32 @@ public class Main {
         System.out.print("Enter a String: ");
         String userInput = sc.nextLine();
         System.out.println("You entered: " + userInput);
-        String quote = "\"";
-        System.out.println("You Entered " + quote + userInput + quote);
+
+//        Add quotes to string
+        System.out.println("You Entered " + convertString(userInput));
 
 //        Check if input is number
-        if (StringUtils.isNumeric(userInput)){
-            System.out.println(quote + userInput + quote + " is a number");
-        } else {
-            System.out.println(quote + userInput + quote + " is not a number");
-        }
+        System.out.println(isNumerical(userInput));
+
 //        Flipped Case
         System.out.println("Flipped Case: " + StringUtils.swapCase(userInput));
 //        Reversed
         System.out.println("Reversed String: " + StringUtils.reverse(userInput));
 //        Contains Vowels Upper/lower
         System.out.println(StringUtils.containsAny(userInput, 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+    }
 
+    //    Excessive Method
+    private static String isNumerical(String string){
+        if (StringUtils.isNumeric(string)){
+            return "\"" + string + "\"" + " is a number";
+        } else {
+            return "\"" + string + "\"" + " is not a number";
+        }
+    }
+
+
+    private static String convertString(String string){
+        return "\"" + string + "\"";
     }
 }
